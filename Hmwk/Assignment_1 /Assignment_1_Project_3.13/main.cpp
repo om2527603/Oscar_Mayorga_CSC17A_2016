07c4e1fcf3f5a741806e01fd1a90842af7a84dda
om2527603 <omayorga1@student.rcc.edu>
2016-02-29 19:28:43 -0800

    Work on Assignment_1

---
+++ Hmwk/Assignment_1 /Assignment_1_Project_3.13/main.cpp
@@ -0,0 +1,44 @@
+/* 
+ * File:   main.cpp
+ * Author: OscJos
+ *
+ * 
+ * 
+ * Created on February 29, 2016, 6:23 PM
+ */
+
+//Libraries
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+//1 US Dollar equals 0.92 Euros 
+//1 US Dollar equals 112.37 Japanese Yen
+    
+    //Declare Variables 
+    const float YEN_PER_DOLLAR=112.37; 
+    const float EUROS_PER_DOLLAR=0.92; 
+    float yen; 
+    float euros;
+    float dollars; 
+    
+    //Ask user to Enter the amount of Dollars they want to convert 
+    cout<< "Enter the Amout of U.S.A Dollars you want to convert: " <<endl;
+    cin>>dollars;
+    
+    //Convert Dollars to YENS AND EUROS 
+    yen=dollars*YEN_PER_DOLLAR;
+    euros=dollars*EUROS_PER_DOLLAR; 
+    
+    //Output the results 
+    cout<<"Total in yens: " <<setprecision(2)<<fixed<<yen <<endl;
+    cout<<"Total in Euros: "<<setprecision(2)<<fixed<<euros<<endl;
+    
+    return 0;
+}
+
