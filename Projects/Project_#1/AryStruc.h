a88b55d1dca98570a78b839bba4998c78d63b437
om2527603 <omayorga1@student.rcc.edu>
2016-05-04 13:21:59 -0700

    Project#1

---
+++ Projects/Project_#1/AryStruc.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   AryStruc.h
+ * Author: Oscar
+ *
+ * Created on April 7, 2016, 1:25 PM
+ */
+
+#ifndef ARYSTRUC_H
+#define	ARYSTRUC_H
+
+//Game 1
+struct Array{
+    int *cup;
+    int size;
+};
+
+//Game 2
+struct Array2{
+    char store;
+};
+
+
+
+#endif	/* ARYSTRUC_H */
