cc9de8034fd3444cb7e791f141ae9addcdbd32a7
om2527603 <omayorga1@student.rcc.edu>
2016-05-11 20:57:23 -0700

    Project_#2

---
+++ Projects/Project_#2/main.cpp
@@ -0,0 +1,27 @@
+/* 
+ * File:   main.cpp
+ * Author: OscJos
+ *
+ * Created on May 11, 2016, 11:18 AM
+ */
+
+//Libraries
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+//function prototypes
+void GameRules();
+
+
+int main(int argc, char** argv) {
+    
+    
+
+    return 0;
+}
+
